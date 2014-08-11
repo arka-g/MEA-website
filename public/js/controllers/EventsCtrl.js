4bd440a5a33e6925f10bc073e70a59c211d162a9
Arka Ganguli <arkaganguli1@gmail.com>
2014-08-10 21:56:26 -0400

    minor refactor css

--- public/js/controllers/EventsCtrl.js
+++ public/js/controllers/EventsCtrl.js
@@ -1,4 +1,7 @@
 angular.module('EventsCtrl', []).controller('EventsController', function($scope) {
 	$scope.welcome='Check out our upcoming and past events!';
-	$scope.subHead="We don't bite"
+	$scope.subHead="We don't bite";
+
+	$scope.upcoming='Upcoming Events';
+	$scope.past='Past Events';
 });
