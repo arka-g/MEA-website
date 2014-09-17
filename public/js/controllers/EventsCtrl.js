edc1a4996aa2ac81da40c90cdc9f276b7e84d269
Arka Ganguli <arkaganguli1@gmail.com>
2014-09-17 10:52:32 -0400

    added carousel in events page

--- public/js/controllers/EventsCtrl.js
+++ public/js/controllers/EventsCtrl.js
@@ -4,4 +4,19 @@ angular.module('EventsCtrl', []).controller('EventsController', function($scope)
 
 	$scope.upcoming='Upcoming Events';
 	$scope.past='Past Events';
+
+	$scope.Eventhead1='First General Meeting';
+	$scope.Eventhead2='Web Dev. Workshop';
+	$scope.Eventhead3='Guest Speaker';
+
+	$scope.infoEvent1 = 'When: September 29, 2014';
+	$scope.subinfoEvent1 = 'Where: ETB 500';
+	$scope.timeEvent1 = 'Time: 5pm';
+    $scope.infoEvent2 = 'When: October 18, 2014';
+    $scope.subinfoEvent2 = 'Where: 1280 Bar & Grill';
+    $scope.timeEvent2 = 'Time: 7pm';
+    $scope.infoEvent3 = 'When: November 8, 2014';
+    $scope.subinfoEvent3 = 'Where: ETB 550';
+    $scope.timeEvent3 = 'Time: 5pm';
+
 });
