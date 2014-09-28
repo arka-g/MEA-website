247fe2ddb7efb8bf2ffda1fc6ee15dfb3dde6fc2
Arka Ganguli <arkaganguli1@gmail.com>
2014-09-28 15:34:46 -0400

    removed all controller

--- public/js/controllers/AllCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('MainCtrl', 'AboutCtrl', 'EventsCtrl', 'TeamCtrl' []).controller('MainController', 'TeamController','EventsController', 'AboutController', function($scope) { 
-    $scope.twitter='https://twitter.com/McMasterEntreps';
-    $scope.facebook='https://www.facebook.com/MacEntrepreneurship';
-    $scope.email='mcmasterentrepreneurship@gmail.com';
-});
