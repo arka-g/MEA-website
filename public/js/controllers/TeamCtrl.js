cd9dcefa228d18addf3da8a140bee8847d11b0a4
Arka Ganguli <arkaganguli1@gmail.com>
2014-09-29 11:46:17 -0400

    took off a member from team page

--- public/js/controllers/TeamCtrl.js
+++ public/js/controllers/TeamCtrl.js
@@ -11,18 +11,16 @@ angular.module('TeamCtrl', []).controller('TeamController', function($scope) {
 	$scope.head4="Daniel D'Cruz";
 	$scope.head5='Arka Ganguli';
 	$scope.head6='Dillon Mulcahy';
-	$scope.head7='John Najafi';
 	$scope.head8='Graham Spry';
 	$scope.head9='Andrew Ramadori';
 
   $scope.info1 = 'President';
   $scope.info2 = 'Co-Vice President';
   $scope.info3 = 'Co-Vice President';
- 	$scope.info4 = 'VP Marketing';
+ 	$scope.info4 = 'VP Strategic Relations';
 	$scope.info5 = 'VP Technical';
   $scope.info6 = 'VP Events';
- 	$scope.info7 = 'VP Strategic Relations';
-  $scope.info8 = 'VP Social Media';
+  $scope.info8 = 'VP Marketing';
   $scope.info9 = 'VP Finance';
 
   $scope.connect = 'Connect With Us'
