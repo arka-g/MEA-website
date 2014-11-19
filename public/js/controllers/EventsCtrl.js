176a2967a722439f360f8253c04e6b3fca6b178e
Arka Ganguli <arkaganguli1@gmail.com>
2014-11-19 12:14:48 -0500

    Added wordpress event

--- public/js/controllers/EventsCtrl.js
+++ public/js/controllers/EventsCtrl.js
@@ -5,16 +5,16 @@ angular.module('EventsCtrl', []).controller('EventsController', function($scope)
 	$scope.upcoming='Upcoming Events';
 	$scope.past='Past Events';
 
-	$scope.Eventhead1='Idea Validation Workshop';
-	$scope.Eventhead2='WordPress-ing Like a Boss';
+	$scope.Eventhead1='Build Your Website With WordPress';
+	$scope.Eventhead2='To Be Announced!';
 	//$scope.subEventhead2 = 'Presented by: SBEC (Small Business Enterprise Center)';
 	$scope.Eventhead3='Off The Record';
 
-	$scope.infoEvent1 = 'When: November 11, 2014';
+	$scope.infoEvent1 = 'When: November 25, 2014';
 	$scope.subinfoEvent1 = 'Where: ETB 533';
-	$scope.timeEvent1 = 'Time: 5:00pm - 7:00pm';
+	$scope.timeEvent1 = 'Time: 6:30pm - 8:30pm';
 
-    $scope.infoEvent2 = 'When: Mid November';
+    $scope.infoEvent2 = 'When: Late November';
     $scope.subinfoEvent2 = 'Where: ETB 535';
     $scope.timeEvent2 = 'Time: TBD';
 
