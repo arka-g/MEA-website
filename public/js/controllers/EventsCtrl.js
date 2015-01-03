abe83730cd13c77a756de36db9f23cdd223852d8
Arka Ganguli <arkaganguli1@gmail.com>
2015-01-03 01:28:28 -0500

    added new events for 2015

--- public/js/controllers/EventsCtrl.js
+++ public/js/controllers/EventsCtrl.js
@@ -1,26 +1,50 @@
 angular.module('EventsCtrl', []).controller('EventsController', function($scope) {
-	$scope.welcome='Check out our upcoming and past events!';
+	$scope.welcome='Check out our upcoming [2015] and past events!';
 	$scope.subHead="We don't bite";
 
-	$scope.upcoming='Upcoming Events';
+	$scope.upcoming='Upcoming Events for 2015!';
 	$scope.past='Past Events';
 
-	$scope.Eventhead1='Build Your Website With WordPress';
-	$scope.Eventhead2='To Be Announced!';
-	//$scope.subEventhead2 = 'Presented by: SBEC (Small Business Enterprise Center)';
-	$scope.Eventhead3='Off The Record';
+	$scope.Eventhead1='Clubsfest!';
+	$scope.Eventhead2='General Meeting';
+	$scope.Eventhead3='Social Media Workshop';
+	$scope.Eventhead4='Kickstarter Workshop';
+	$scope.Eventhead5='Government Grants';
+	$scope.Eventhead6='Website Workshop';
+	$scope.Eventhead7='Food Startups';
+	$scope.Eventhead8='Off The Record';
 
-	$scope.infoEvent1 = 'When: November 25, 2014';
-	$scope.subinfoEvent1 = 'Where: ETB 533';
-	$scope.timeEvent1 = 'Time: 6:30pm - 8:30pm';
+	$scope.infoEvent1 = 'When: January 7/8';
+	$scope.subinfoEvent1 = 'Where: TBD';
+	$scope.timeEvent1 = 'Time: TBD';
 
-    $scope.infoEvent2 = 'When: Late November';
+    $scope.infoEvent2 = 'When: January 13';
     $scope.subinfoEvent2 = 'Where: ETB 535';
-    $scope.timeEvent2 = 'Time: TBD';
+    $scope.timeEvent2 = 'Time: 6pm - 8pm';
 
-    $scope.infoEvent3 = 'When: Weekly';
-    $scope.subinfoEvent3 = 'Where: Starbucks/ Coffee Shop';
-    $scope.timeEvent3 = 'Time: TBD. Sign up by clicking the icon above';
+    $scope.infoEvent3 = 'When: January 20';
+    $scope.subinfoEvent3 = 'Where: ETB 535';
+    $scope.timeEvent3 = 'Time: 6pm - 8pm';
+
+    $scope.infoEvent4 = 'When: February 3';
+    $scope.subinfoEvent4 = 'Where: ETB 535';
+    $scope.timeEvent4 = 'Time: 6pm - 8pm';
+
+   	$scope.infoEvent5 = 'When: February 10';
+    $scope.subinfoEvent5 = 'Where: ETB 535';
+    $scope.timeEvent5 = 'Time: 6pm - 8pm';
+
+ 	$scope.infoEvent6 = 'When: March 3';
+    $scope.subinfoEvent6 = 'Where: TBD';
+    $scope.timeEvent6 = 'Time: TBD';
+
+	$scope.infoEvent7 = 'When: March 10';
+    $scope.subinfoEvent7 = 'Where: ETB 535';
+    $scope.timeEvent7 = 'Time: 6pm - 8pm';
+
+  	$scope.infoEvent8 = 'When: Weekly';
+    $scope.subinfoEvent8 = 'Where: Starbucks/ Coffee Shop';
+    $scope.timeEvent8 = 'Time: TBD. Sign up by clicking the icon above';
 
     $scope.twitter='https://twitter.com/McMasterEntreps';
   	$scope.facebook='https://www.facebook.com/MacEntrepreneurship';
