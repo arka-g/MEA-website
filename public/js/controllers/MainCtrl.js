1be471affd2e07c8118fe93986e6688345fa0787
Arka Ganguli <arkaganguli1@gmail.com>
2015-02-15 22:27:52 -0500

    adding positions for executive team 2015/2016 page

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -8,7 +8,44 @@ angular.module('MainCtrl', []).controller('MainController', function($scope) {
 	$scope.head1='Have an idea?';
 	$scope.head2='Grow Your Network';
 	$scope.head3='Learn';
+    //temporary for executive positions
+    $scope.Pres1='Preside over all meetings of the club and shall be an ex-officio member of all committees within the club';
+    $scope.Pres2='Call all executive meetings and general meetings';
+    $scope.Pres3='Handle the day-to-day operations of the club';
+    $scope.Pres4='Ensure the club’s adherence to MSU policies';
+    $scope.Pres5='Manage other executives and team members to hold accountable for tasks and initiatives';
+    $scope.Pres6='Co-run meetings with Vice-President';
+    $scope.Pres7='Build relationships with strategic partners';
+    $scope.Pres8='Maintain the club’s membership list and archival documents';
+    $scope.Pres9='Manage email list, and newsletters Vice-President';
 
+
+    $scope.VP1='Perform the duties of the President in the latters absence and shall assist the President where required';
+    $scope.VP2='Keep the minutes of all official meetings and be responsible for all correspondence of the association';
+    $scope.VP3='Perform such other duties that are from time to time assigned by the President, executive or general membership';
+    $scope.VP4='Support president and all executives and team members with their tasks; cross-collaborative and assists and fills in gaps where needed';
+    $scope.VP5='Co-run meetings with President';
+
+    $scope.Finance1='Keep the account books of the club, shall arrange for the custody and distribution of funds pursuant to the executive and general memberships direction';
+    $scope.Finance2='Give a report to the finances of the club at each executive meeting and have the books available upon the request of any member of the club';
+    $scope.Finance3='Perform such other duties that are from time to time assigned by the President, executive or general membership';
+    $scope.Finance4='Responsible for budgeting, forecasting, setting event prices (along with President)';
+    $scope.Finance5='Handles all banking, MSU obligations';
+
+    $scope.Marketing1='Perform such other duties that are from time to time assigned by the President, executive or general membership';
+    $scope.Marketing2='Handles Marketing designs';
+    $scope.Marketing3='Manages event sign-ups';
+    $scope.Marketing4='Manages advertising for events (traditional)';
+    $scope.Marketing5='Be in charge of all social media marketing for events';
+    $scope.Marketing6='Manage interactions on social media, by answering questions, and building new relations';
+    $scope.Marketing7='Provide photo and video rich content';
+
+    $scope.Tech1='Manage website content';
+    $scope.Tech2='Keep website, and other McMaster calendars up to date';
+    $scope.Tech3='Provide information pages for sponsors';
+    $scope.Tech4='Provide sign up forms, and polls';
+
+    //end
     $scope.info1 = 'We can help bring your ideas to life by providing you with the right guidance';
     $scope.info2 = 'We provide access to many events with top entrepreneurs';
     $scope.info3 = 'We hold workshops that teach web design, pitching skills, and more';
